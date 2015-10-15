c45d94b31cd7365c20096839d481748c334b7524
Elisabeth Bommes <elisabeth.bommes@hu-berlin.de>
2015-10-15 10:39:03 +0200

    Additional comments and one style guide change

--- TXTBreweries/TXTBreweries.R
+++ TXTBreweries/TXTBreweries.R
@@ -19,7 +19,7 @@ data.df = readRDS("TXTGermanBrewerieslonlat.RDS")
 data.df$Avg         = as.numeric(data.df$Avg)
 data.df             = data.df[data.df$Avg != 0,]
 data.df$NoBeer      = as.numeric(data.df$NoBeer)
-data.df$NoBeer.size = data.df$NoBeer/max(data.df$NoBeer)*20 + 1
+data.df$NoBeer.size = data.df$NoBeer/max(data.df$NoBeer) * 20 + 1
 
 # Classes for color coding
 data.df$Avg.class                     = 0
@@ -27,12 +27,13 @@ data.df$Avg.class[data.df$Avg >  4]   = 1
 data.df$Avg.class[data.df$Avg <= 4]   = 2
 data.df$Avg.class[data.df$Avg <= 3.5] = 3
 data.df$Avg.class[data.df$Avg <= 3]   = 4
+data.df$Avg.class                     = as.factor(data.df$Avg.class)
 
-data.df$Avg.class = as.factor(data.df$Avg.class)
+# Color palette
 pal = colorFactor(colorRampPalette( c("green", "red"), space = "rgb")(4),
                   domain = c("1", "2", "3", "4"))
 
-
+# Plot
 m  =  leaflet(data = data.df) %>% setView(lng = 10.3833, lat = 50.5167, zoom = 6)
 m %>% addProviderTiles("OpenStreetMap.HOT") %>%
       addCircleMarkers(lng = ~lon, lat = ~lat, radius = ~data.df$NoBeer.size,
